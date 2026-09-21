d0ccc2344e69382a1ae7ad6b936b844fb27f0451
Joni Dani <jonidani06@gmail.com>
2026-09-21 21:00:01 +0200

    removing eslintrc

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -4,14 +4,17 @@ const js = require("@eslint/js");
 module.exports = [
   js.configs.recommended,
   {
+    ignores: ["docs/**"]  // ← ignore frontend files
+  },
+  {
     languageOptions: {
       ecmaVersion: "latest",
       sourceType: "commonjs",
       globals: {
         ...globals.node,
+        ...globals.jest,    // ← adds jest globals
       }
     },
-    rules: {
-    }
+    rules: {}
   }
 ];
